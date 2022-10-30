3f405c8439c05e98c0d95cad8c3211ce0003b635
killerbox04 <killerbox04@gmail.com>
2022-10-30 17:33:45 -0400

    takes arguments for cache values

--- streamlinkLauncher.bat
+++ streamlinkLauncher.bat
@@ -1,14 +1,15 @@
-@echo off
 
 
 set "in=%1"
+set "fileCache=%2"
+set "netCache=%3"
 
 :A
 
 set "const=twitch.tv/"
 set "req=%const%%in%"
 
-streamlink -a "--file-caching=2000 --network-caching=2000" %req% best
+streamlink -a "--file-caching=%fileCache% --network-caching=%netCache%" %req% best
 
 
 python maintk.pyw
